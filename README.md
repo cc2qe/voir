4da76529add41793ab7bf832e7dacad1371cc655
Colby Chiang <cc2qe@virginia.edu>
2014-11-08 14:43:39 -0600

    added scatter plot

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Tools for viewing data
 
 ## Table of contents
 * [histogram.R](#histogramr)
+* [scatter.R](#scatterr)
 
 ### histogram.R
 
@@ -23,3 +24,22 @@ positional arguments:
 histogram.R insert_size.txt
 ```
 ![Example histogram](examples/figures/insert_size.txt.png?raw=true "Example histogram")
+
+### scatter.R
+
+Plot a scatter plot from two columns of numbers
+
+#### Usage
+```
+usage: scatter.R <file>
+
+positional arguments:
+  file               File with two columns of numerical values,
+                       tab-delimited.
+```
+
+#### Example
+```
+scatter.R two_col.txt
+```
+![Example scatter plot](examples/figures/two_col.txt.png?raw=true "Example scatter plot")
---
+++ scatter.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+# print usage
+usage <- function() {
+	cat(
+'usage: scatter.R <file>
+
+scatter.R
+author: Colby Chiang (cc2qe@virginia.edu)
+description: Plot a scatter plot from two columns of numbers
+
+positional arguments:
+  file               File with two columns of numerical values,
+                       tab-delimited.
+')
+}
+
+# Draw a histogram from a text file
+args <- commandArgs(trailingOnly=TRUE)
+file <- args[1]
+filename <- basename(args[1])
+
+# Check input args
+if (is.na(file)) {
+	usage()
+	quit(save='no', status=1)
+}
+
+x <- matrix(scan(file, what='raw', sep='\t'), byrow=TRUE, ncol=2)
+
+# pdf(paste0(basename(args[1]), '.pdf'), height=5, width=5)
+png(paste0(basename(args[1]), '.png'), height=5, width=5, units='in', res=150)
+plot(x[,1], x[,2], main=paste0('Histogram of ', filename), xlab=paste(filename, 1), ylab=paste(filename, 2))
+dev.off()
